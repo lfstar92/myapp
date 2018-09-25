45d4c552ccca45ae6b22f0650cd3e1c2311b4102
xinyixue666 <xinyixue666@163.com>
2018-09-25 15:41:30 +0800

    增加登录路由

---
+++ routes/login.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/',(req, res, next) => {
+  res.render('login', {
+    title: 'login'
+  })
+})
+
+module.exports = router;
