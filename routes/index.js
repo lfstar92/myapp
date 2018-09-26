86f15fc3e6e391bd6d6aa7860d3882ffb58abbfe
xinyixue666 <xinyixue666@163.com>
2018-09-26 11:53:07 +0800

    修改传参

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    linkList: [{
+      href: '/',
+      name: "首页",
+      active: true
+    }, {
+      href: '/article',
+      name: "文章",
+      active: false
+    }, {
+      href: '/list',
+      name: "列表",
+      active: false
+    }, {
+      href: '/about',
+      name: "关于",
+      active: false
+    }]
+  });
 });
 
 module.exports = router;
