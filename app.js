5e9472e20c11ae05815f81328ca8937cee6ac2cf
xinyixue666 <xinyixue666@163.com>
2018-09-26 11:54:08 +0800

    添加新路由渲染路径

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var articleRouter = require('./routes/article');
+var articleListRouter = require('./routes/articleList');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/article', articleRouter);
+app.use('/list', articleListRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
